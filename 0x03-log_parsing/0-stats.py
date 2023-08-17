8b8f546eba9b1cec73829c6cca258c6467084441
Budaboss <opiyocaroline20@gmail.com>
2023-08-17 22:16:10 +0300

    0x03-log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,68 +1,46 @@
 #!/usr/bin/python3
-"""Write a script that reads stdin line by line and computes metrics:
+""" script that reads stdin line by line and computes metrics """
 
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size> (if the format is not this one, the line
-must be skipped)
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-print these statistics from the beginning:
-Total file size: File size: <total size>
-where <total size> is the sum of all previous <file size>
-(see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear or is not an integer,
-don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
+from sys import stdin
 
-line list = [<IP Address>, -, [<date>], "GET /projects/260 HTTP/1.1",
-<status code>, <file size>]
-"""
-
-
-import sys
-
-# store the count of all status codes in a dictionary
-status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0}
-
-total_size = 0
-count = 0  # keep count of the number lines counted
+# init vaariables
+total_file_size = 0
+status_code_count_map = {}
 
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
+    # loop through the lines from the keyboard input
+    for line_no, line in enumerate(stdin, start=1):
+        line = line.strip()
 
-        if len(line_list) > 4:
-            status_code = line_list[-2]
-            file_size = int(line_list[-1])
+        # tokenize the line read
+        line_token: list = line.split()
+        # print(line_token)  # for debug
+        # print(len(line_token))
+        if len(line_token) != 9:
+            continue
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
-            if status_code in status_codes_dict.keys():
-                status_codes_dict[status_code] += 1
+        # destructure the tokenized line
+        ip_address, _, _, _, _, _, request, status_code, file_size = line_token
 
-            # update total size
-            total_size += file_size
+        try:
+            status_code = int(status_code)
+        except ValueError:
+            continue
 
-            # update count of lines
-            count += 1
+        total_file_size += int(file_size)
 
-        if count == 10:
-            count = 0  # reset count
-            print('File size: {}'.format(total_size))
+        if status_code in status_code_count_map:
+            status_code_count_map[status_code] += 1
+        else:
+            status_code_count_map[status_code] = 1
 
-            # print out status code counts
-            for key, value in sorted(status_codes_dict.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        if line_no % 10 == 0:
+            print(f"File size: {total_file_size}")
+            for code in sorted(status_code_count_map.keys()):
+                print(f"{code}: {status_code_count_map[code]}")
 
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_codes_dict.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+except KeyboardInterrupt as err:
+    print(f"File size: {total_file_size}")
+    for code in sorted(status_code_count_map.keys()):
+        print(f"{code}: {status_code_count_map[code]}")
+    print(err)
